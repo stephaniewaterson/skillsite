e238aa570c6f571a71ab78a135b4232643b47262
Stephanie Waterson <stephaniemariewaterson@gmail.com>
2026-03-06 19:18:25 +0000

    gap

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,7 @@ const layout = isMobile
       projectsX: 25,
       projectsJump: 16.75,
       skillsX: 36,
-      skillsJump: 27,
+      skillsJump: 27.75,
       projectsY: 6,
       skillsY: 8,
       showSpaceMan: false,
