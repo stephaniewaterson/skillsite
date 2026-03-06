9467ea230c0fb8c221eb25916f3a85dc9f73bd9d
Stephanie Waterson <stephaniemariewaterson@gmail.com>
2026-03-06 19:29:10 +0000

    22.5

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,7 @@ const layout = isMobile
       projectsX: 25,
       projectsJump: 16,
       skillsX: 36,
-      skillsJump: 23.5,
+      skillsJump: 22.5,
       projectsY: 6,
       skillsY: 8,
       showSpaceMan: false,
